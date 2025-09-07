5f95fc8e1c8e9defafd10138818972e2cecb199d
24Bhuvan <ubhuvan533@gmail.com>
2025-09-07 18:54:57 +0530

    # Special Functions for Creating Arrays

---
+++ PythonLibrariesLab/DataManipulation/Numpy/SpecialFuncs.py
@@ -0,0 +1,7 @@
+import numpy as np
+np.zeros((3, 4))  # Creates a 3x4 array of zeros
+np.ones((2, 3, 4), dtype=np.int16)  # Creates a 2x3x4 array of ones with int16 data type
+np.empty((2, 3))  # Creates a 2x3 array with uninitialized values
+np.arange(10, 30, 5)  # Creates an array with values from 10 to 30 with a step of 5
+np.linspace(0, 2, 9)  # Creates an array of 9 evenly spaced values from 0 to 2
+np.eye(3)  # Creates a 3x3 identity matrix
